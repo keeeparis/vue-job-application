4d00f5f361987a28fef68d67b49c637de6177da4
Keeeparis <vladimir1trotsenko@gmail.com>
2022-04-08 12:23:04 +0600

    comments

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from "./App.vue";
 
 const app = createApp(App);
 
+// подключаем vue store для state management
 app.use(createPinia());
 
 app.mount("#app");
--- src/stores/todo.ts
+++ src/stores/todo.ts
@@ -1,12 +1,13 @@
 import { defineStore } from "pinia"
 import { v4 } from 'uuid'
 
+// типы для стэйта и туду
 interface Props {
   todo: Todo[],
   filter: 'all' | 'active' | 'completed'
 }
 
-type Todo = {
+export type Todo = {
   active: boolean,
   id: number | string,
   text: string
@@ -19,11 +20,16 @@ export const useTodoStore = defineStore({
     filter: 'all'
   }),
   actions: {
+    // асинхронный метод загрузки туду с внешнего API
+    // и замены state.todo на результат запроса
     async loadTodos() {
       const response = await fetch('https://my-json-server.typicode.com/falk20/demo/todos')
       const json = await response.json()
       this.todo = json
     },
+    // метод добавления нового туду в state.todo
+    // создаем объект по типу Todo и методом push 
+    // помещаем в конец массива
     addTodo(mes: string) {
       const newTodo = {
         active: false,
@@ -32,8 +38,16 @@ export const useTodoStore = defineStore({
       }
       this.todo.push(newTodo)
     },
+    // метод установки нового значения фильтра по 
+    // переданному аргументу
     setFilter(category: Props['filter']) {
       this.filter = category
+    },
+    // метод удаления туду из state.todo путем
+    // вызова метода массива filter, который 
+    // возвращает все туду с отличным от аргумента id
+    deleteTodo(id: Todo['id']) {
+      this.todo = this.todo.filter(item => item.id !== id)
     }
   },
 })
